d7ddc1cd2a108a68a64ee8aecc3666a151be5894
Prikshit singh <prikshitsingh79@gmail.com>
2025-01-04 18:34:06 +0530

    Improve navbar ui

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,13 +4,13 @@ export default function Navbar({employeeId}:{employeeId:string}){
     return(
         <nav>
             <ul>
-                <li style={{marginBottom:'10px',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
+                <li style={{marginBottom:'10px', cursor:'pointer',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
                     <NavLink to={`/dashboard/${employeeId}/home`}>Home</NavLink>
                 </li>
-                <li style={{marginBottom:'10px',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
+                <li style={{marginBottom:'10px', cursor:'pointer',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
                     <NavLink to={`/dashboard/${employeeId}/buying`}>Buying</NavLink>
                 </li>
-                <li style={{marginBottom:'10px',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
+                <li style={{marginBottom:'10px', cursor:'pointer',borderRadius:'6px',paddingLeft:'2px',height:'25px',marginRight:'40px'}}>
                     <NavLink to={`/dashboard/${employeeId}/accounting`}>Accounting</NavLink>
                 </li>
             </ul>
