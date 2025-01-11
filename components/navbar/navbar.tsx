3c685c4c5c663573eb27c86091396e5082ff304b
Prikshit singh <prikshitsingh79@gmail.com>
2025-01-11 16:55:00 +0530

    Adjust hieght and weight of icons

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,26 +8,26 @@ export default function Navbar({userId}:{userId:string}){
     return(
     <div className="flex flex-col" style={{marginRight:'30px'}}>
         <NavLink to={`/dashboard/${userId}/home`} className="block py-2">
-            <div className="cursor-pointer rounded-md h-6" style={{fontSize:'15px',display:'flex', flexDirection:'row',justifyContent:'start',alignItems:'end'}}>
-                <div><img src={homeIcon} alt="" className="w-6"/></div>
+            <div className="cursor-pointer rounded-md h-6" style={{fontSize:'15px',display:'flex', flexDirection:'row',justifyContent:'start'}}>
+                <div><img src={homeIcon} alt="" className="w-5"/></div>
                 <div>Home</div>
             </div>
         </NavLink>
         <NavLink to={`/dashboard/${userId}/buying`} className="block py-2">
-            <div className="cursor-pointer rounded-md  h-6" style={{fontSize:'15px',display:'flex',flexDirection:'row',justifyContent:'start',alignItems:'center'}}>
+            <div className="cursor-pointer rounded-md  h-6" style={{fontSize:'15px',display:'flex',flexDirection:'row',justifyContent:'start'}}>
                 <div><img src={buyingIcon} alt="" className="w-5"/></div>
                 Buying
             </div>
         </NavLink>
         <NavLink to={`/dashboard/${userId}/accounting`} className="block py-2">
-            <div className="cursor-pointer rounded-md  h-6" style={{fontSize:'15px',display:'flex',justifyContent:'start',alignItems:'center'}}>
+            <div className="cursor-pointer rounded-md  h-6" style={{fontSize:'15px',display:'flex',justifyContent:'start'}}>
                 <div><img src={accountingIcon} alt="" className="w-5"/></div>
                 Accounting
             </div>
         </NavLink>
         <NavLink to={`/dashboard/${userId}/users`} className="block py-2">
             <div className="cursor-pointer rounded-md h-6" 
-            style={{fontSize:'15px',display:'flex',justifyContent:'start',alignItems:'center'}}>
+            style={{fontSize:'15px',display:'flex',justifyContent:'start'}}>
                 <div><img src={userIcon} alt="" className="w-5"/></div>
                 Users
             </div>
